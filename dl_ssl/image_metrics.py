626778ee9918858ef0145967f6351f6e1dbcf9ea
SridharPandian <sripan172@gmail.com>
2022-04-10 19:56:56 -0400

    Training works!

--- dl_ssl/image_metrics.py
+++ dl_ssl/image_metrics.py
@@ -1,24 +1,35 @@
 # Standard imports
 import argparse
 
+# Torch imports
+import torchvision.transforms as T
+
 # Dataset and Function imports
-from dl_ssl.datasets.respresentation import RepresentationImageDataset
-from dl_ssl.utils.normalize import obtain_normal_values
+from dl_ssl.datasets.dataset import *
+from dl_ssl.utils.normalize import get_normal_values
+
+# Other utility imports
+from dl_ssl.utils.files import get_path_in_package
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--labelled', action='store_true')
 parser.add_argument('--unlabelled', action='store_true')
 
+def get_transform():
+    transforms = []
+    transforms.append(T.ToTensor())
+    return T.Compose(transforms)
+
 if __name__ == "__main__":
     options = parser.parse_args()
-
+    
     # Getting metrics for unlabelled data
-    if options.labelled is True:    
-        dataset = RepresentationImageDataset(224, '/home/sridhar/.personal/deep-learning/project/dl_ssl/data/unlabelled_data')
-        obtain_normal_values(dataset, 224, 224)
+    if options.unlabelled is True:    
+        dataset = UnlabeledDataset(root = get_path_in_package('data/unlabeled_data'), img_size = 224)
+        get_normal_values(dataset, 224, 224)
 
     # Getting metrics for labelled data
     if options.labelled is True:    
-        dataset = RepresentationImageDataset(224, '/home/sridhar/.personal/deep-learning/project/dl_ssl/data/labelled_data/training')
-        obtain_normal_values(dataset, 224, 224)
+        dataset = UnlabeledDataset(root = get_path_in_package('data/labeled_data/training/images'), img_size = 224, unlabelled = False)
+        get_normal_values(dataset, 224, 224)
